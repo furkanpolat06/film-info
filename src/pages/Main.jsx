491a3d606477137c49a581c53157455208d7585d
furkanpolat06 <emnplt.0698@gmail.com>
2023-07-29 17:18:52 +0300

    finish project

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,53 +3,84 @@ import Button from "@mui/material/Button";
 import { data } from "../helper/data";
 import { useState } from "react";
 import StarRank from "../components/StarRank";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Box, ButtonBase, CardActionArea } from "@mui/material";
 
 const Main = () => {
   const [yearItem, setYearItem] = useState("");
+  const [clickedButtonId, setClickedButtonId] = useState(null);
+
 
   const handleClick = (item) => {
     setYearItem(item);
+    setClickedButtonId(item.id)
+    
   };
   console.log(yearItem);
 
   return (
-    <Stack  sx={{ display: "flex" }} spacing={2}>
-      <div>
-      {data.map((item) => (
-          <Button key={item.id} onClick={() => handleClick(item)} variant="outlined">
-        {item.date}
-      </Button>
-      ))}
-      </div>
-      <div>
+    <Box sx={{ display: "flex", flexDirection: "row", marginLeft:"20vw",  marginTop:"10vw",  }} spacing={2}>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        {data.map((item) => (
+          <Button 
+            sx={{ maxWidth: 70, marginBottom: "12px",  marginRight: "15px",  marginTop: "15px",
+             
+            "&:hover": {backgroundColor: "lightgray", border:"none"} ,
+            
+            border:"none",
+            backgroundColor:"lightgray",
+            fontWeight:"bold",
+            fontSize:"1rem",
+            
+            borderBottom: clickedButtonId === item.id ? '3px solid red' : 'initial' ,
+            color: clickedButtonId === item.id ? 
+            'red' : 'initial',
+            
 
-      {yearItem && (
-        <Card sx={{ maxWidth: 500 }} >
-          <CardActionArea >
-            <CardContent >
-              <Typography gutterBottom variant="h5" component="div">
-                {yearItem.title}
-              </Typography>
-              <Typography variant="body2">
-                {yearItem.body}
-              </Typography>
-              <Typography variant="body2" color="red">
-                {yearItem.tags.join(" / ")}
-              </Typography>
-              <Typography spacing={2} variant="body2" color="red" sx={{ display: "flex", alignItems: "center" }}>
-                <StarRank  filmRate={yearItem.rate} />  
-                <Typography sx={{fontSize:"h5.fontSize", marginLeft: "15px" } }> {yearItem.rate.toFixed(2)} / 10 </Typography>
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      )}
-      </div>
-    </Stack>
+          }} 
+            key={item.id}
+            onClick={() => handleClick(item)}
+            variant="outlined"
+          >
+            {item.date}
+          </Button>
+        ))}
+      </Box>
+
+      <Box >
+        {yearItem && (
+          <Card sx={{ maxWidth: 800, minHeight:240, margin: "10px" }}>
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {yearItem.title}
+                </Typography>
+                <Typography variant="body2">{yearItem.body}</Typography>
+                <Typography variant="body2" color="red">
+                  {yearItem.tags.join(" / ")}
+                </Typography>
+                <Typography
+                  spacing={2}
+                  variant="body2"
+                  color="red"
+                  sx={{ display: "flex", alignItems: "center" }}
+                >
+                  <StarRank filmRate={yearItem.rate} />
+                  <Typography
+                    sx={{ fontSize: "h5.fontSize", marginLeft: "15px" }}
+                  >
+                    {" "}
+                    {yearItem.rate.toFixed(2)} / 10{" "}
+                  </Typography>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        )}
+      </Box>
+    </Box>
   );
 };
 
